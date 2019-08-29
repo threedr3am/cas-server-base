04de90d98f6af2365e61fb6a25612c7cd5ca52f4
xuanyh <xuanyh@dxy.cn>
2019-08-29 18:37:17 +0800

    使用自定义密码编码器

---
+++ src/main/java/com/threedr3am/learn/encoder/CustomPasswordEncoder.java
@@ -0,0 +1,38 @@
+package com.threedr3am.learn.encoder;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author xuanyh
+ */
+@Slf4j
+public class CustomPasswordEncoder implements PasswordEncoder {
+
+  @Override
+  public String encode(CharSequence charSequence) {
+    try {
+      MessageDigest md = MessageDigest.getInstance("MD5");
+      md.update(charSequence.toString().getBytes());
+      String pwd = new BigInteger(1, md.digest()).toString(16);
+      log.info("encode password, before ({}), after ({})", charSequence, pwd);
+      return pwd;
+    } catch (NoSuchAlgorithmException e) {
+      log.error("encode password error", e);
+    }
+    return null;
+  }
+
+  @Override
+  public boolean matches(CharSequence charSequence, String s) {
+    if (StringUtils.isEmpty(charSequence))
+      return false;
+    String pass = encode(charSequence);
+    log.info("request password ({}) encode is ({}), db password is ({})", charSequence, pass, s);
+    return StringUtils.equals(pass, s);
+  }
+}
--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -10,3 +10,6 @@ CREATE TABLE `user_info` (
   UNIQUE KEY `username` (`username`) USING BTREE,
   UNIQUE KEY `id_card_num` (`id_card_num`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+INSERT INTO `user_info` (`uid`,`username`,`password`,`name`,`id_card_num`,`state`) VALUES (4,'admin','e10adc3949ba59abbe56e057f20f883e','一想到你','441283xxxxxxxxxxxx','0');
+INSERT INTO `user_info` (`uid`,`username`,`password`,`name`,`id_card_num`,`state`) VALUES (5,'test','ed0290f05224a188160858124a5f5077','我就哦哦哦哦哦','166666666666666666','0');
